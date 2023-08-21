b101774ed410f3755a1ebc28f507b0d3b316e586
EC2 Default User <ec2-user@ip-172-31-18-189.ap-northeast-1.compute.internal>
2023-08-21 09:29:57 +0000

    [indexのみ完成]

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,18 +1,36 @@
 class Public::CustomersController < ApplicationController
+    before_action :authenticate_customer!
+
   def show
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
   end
-  
+
   def update
-    
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to public_customer_path(@customer), notice: "登録情報が更新されました。"
+    else
+      render :edit
+    end
   end
 
   def check
   end
-  
+
   def withdraw
-    
+
+  end
+
+  private
+
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :address, :postcode, :telephone_number)
+　end
+
   end
 end
