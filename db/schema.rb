c6531429bdf7b645321bc398fe33d983df6396a5
RyuyaGohara <138365197+RyuyaGohara@users.noreply.github.com>
2023-08-23 08:36:51 +0000

    add public-address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_22_051531) do
+ActiveRecord::Schema.define(version: 2023_08_23_071613) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -120,6 +120,7 @@ ActiveRecord::Schema.define(version: 2023_08_22_051531) do
     t.string "postcode"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "customer_id"
   end
 
   create_table "users", force: :cascade do |t|
