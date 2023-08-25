1164159b06868f35326006405f0edbadd8866866
EC2 Default User <ec2-user@ip-172-31-18-189.ap-northeast-1.compute.internal>
2023-08-25 07:42:43 +0000

    [finish}

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+   before_action :set_current_customer
+
+  private
+
+  def set_current_customer
+    @customer = current_customer if customer_signed_in?
+  end
 end
+
